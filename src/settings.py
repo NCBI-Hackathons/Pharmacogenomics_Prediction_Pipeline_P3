3fb4e977c74f2f719f5fc9838843175a652eb956
Uday Evani <usevani@nygenome.org>
2015-08-04 15:50:08 +0000

    using yaml instead of json

--- src/settings.py
+++ src/settings.py
@@ -1,14 +1,14 @@
 import os
-import json
+import yaml
 
 class SettingsParser(object):
-    SETTINGS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'settings.json')
+    SETTINGS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'settings.yaml')
 
     def __init__(self):
         with open(self.SETTINGS_FILE) as fh:
-            self.data = json.load(fh)
+            self.data = yaml.load(fh)
 
 j = SettingsParser()
 print j.SETTINGS_FILE
-print j.data
+print j.data['data']['raw']
 

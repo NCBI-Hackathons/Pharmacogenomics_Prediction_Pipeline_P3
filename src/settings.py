aa4d0d1c018d24442bbb89179b8777b90104f5e4
Ubuntu <ubuntu@ip-172-16-241-12.ec2.internal>
2015-08-04 20:59:00 +0000

    commenting usage lines

--- src/settings.py
+++ src/settings.py
@@ -5,10 +5,18 @@ class SettingsParser(object):
     SETTINGS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'settings.yaml')
 
     def __init__(self):
-        with open(self.SETTINGS_FILE) as fh:
-            self.data = yaml.load(fh)
-
-j = SettingsParser()
-print j.SETTINGS_FILE
-print j.data['data']['raw']
+        self._settings=None
+    
+    @property
+    def settings(self):
+        if self._settings is None:
+            with open(self.SETTINGS_FILE) as fh:
+                self._settings = yaml.load(fh)
+                return self._settings
+        else:
+            return self._settings
+# Usage
+#j = SettingsParser()
+#print j.SETTINGS_FILE
+#print j.settings['data']['raw']
 

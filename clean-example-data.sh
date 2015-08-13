07e6025185e5293f2b4c37d791eaa859e5f0b745
daler <dalerr@niddk.nih.gov>
2015-08-13 12:23:03 -0400

    add script for cleaning example data prior to a new test run

---
+++ clean-example-data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create a fresh copy of example_data
+#
+# Used for testing the pipeline starting from raw data.
+#
+# e.g.,
+#
+#
+#   ./clean-example-data.sh
+#   snakemake -pr -s runall.snakefile
+#
+# To do a full test.
+#
+#
+if [ -e example_data ]; then
+    rm -rf example_data
+fi
+mkdir -p example_data
+(cd example_data && unzip ../raw.zip)

865821218adaafd2a7806781a52d2b2a9a052fd3
daler <dalerr@niddk.nih.gov>
2015-09-30 16:10:41 -0400

    add name resolver function so we can specify filter funcs in config

--- tools/pipeline_helpers.py
+++ tools/pipeline_helpers.py
@@ -115,6 +115,30 @@ def stitch(filenames, sample_from_filename_func, index_col=0, data_col=1,
     return df
 
 
+def resolve_name(name):
+    """
+    Imports a specific object from a dotted path.
+
+    From nose.utils.resolve_name (with the logging parts taken out) which in
+    turn is from unittest.TestLoader.loadTestByName
+    """
+    parts = name.split('.')
+    parts_copy = parts[:]
+    while parts_copy:
+        try:
+            module = __import__('.'.join(parts_copy))
+            break
+        except ImportError:
+            del parts_copy[-1]
+            if not parts_copy:
+                raise
+    parts = parts[1:]
+    obj = module
+    for part in parts:
+        obj = getattr(obj, part)
+    return obj
+
+
 def remove_zero_variance(infile):
     """
     Remove rows with zero variance
